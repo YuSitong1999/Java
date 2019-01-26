3257b3d0ca5c23009c0d46d5fa3462dba11f8485
YuSitong <yusitong1999@qq.com>
2019-01-26 10:50:29 +0800

    第三章 First

---
+++ Chapter 3/First.java
@@ -0,0 +1,9 @@
+
+public class First{
+    static String s1 = "ÄãºÃ";
+    public static void main(String[] args){
+        String s2 = "Java";
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
